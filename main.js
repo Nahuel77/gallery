79ef964876497a55f2f87e1bd0ce87c1c1f84cf1
Nahuel77 <70348604+Nahuel77@users.noreply.github.com>
2020-08-27 18:28:39 -0300

    Update main.js

--- main.js
+++ main.js
@@ -14,26 +14,22 @@ $(document).ready(function(){
 		thispic = $(this);
 		var pic = thispic.attr('src');
 		$('#thispic').attr('src', pic);
-		console.log(thispic);
 		actualPic();
 	}
 
 	function actualPic(){
 		actual = $('#thispic').attr('src');
 		thispic = $('.pic[src*="'+actual+'"]');
-		console.log(thispic);
 	}
 
 	function prevPic(){
 		var pic = thispic.prev().attr('src');
-		console.log(pic);
 		$('#thispic').attr('src', pic);
 		actualPic();
 	}
 
 	function nextPic(){
 		var pic = thispic.next().attr('src');
-		console.log(pic);
 		$('#thispic').attr('src', pic);
 		actualPic();
 	}
